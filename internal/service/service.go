16bba6d5c8756b5bf8eb1cac44d02868fb5a10f8
Dmitriy <dimanfadeev2003@gmail.com>
2026-05-20 21:58:44 +0400

    first

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
@@ -10,11 +11,10 @@ func Service(t string) (string, error) {
 	if len(t) <= 0 {
 		return "", errors.New("пустая строка")
 	}
-	for _, q := range t {
-		if q >= 'А' && q <= 'Я' {
-			text := morse.ToMorse(t)
-			return text, nil
-		}
+	rus := "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+	if strings.ContainsAny(t, rus) {
+		text := morse.ToMorse(t)
+		return text, nil
 	}
 	text := morse.ToText(t)
 	return text, nil
